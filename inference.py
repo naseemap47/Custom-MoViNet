1162fb622eae44a838bbea93d18e56a7beb3d582
NASEEM A P <naseemalassampattil@gmail.com>
2023-03-17 12:30:38 +0530

    successfully inferenced video stream using opencv

--- inference.py
+++ inference.py
@@ -1,37 +1,10 @@
-import os
-
-import tqdm
-import random
-import pathlib
-import itertools
-import collections
-
 import cv2
-import numpy as np
-import remotezip as rz
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-import keras
 import tensorflow as tf
-import tensorflow_hub as hub
-# from tensorflow.keras import layers
-# from tensorflow.keras.optimizers import Adam
-# from tensorflow.keras.losses import SparseCategoricalCrossentropy
-
-import tensorflow_datasets as tfds
-from official.vision.configs import video_classification
-from official.projects.movinet.configs import movinet as movinet_configs
-from official.projects.movinet.modeling import movinet
-from official.projects.movinet.modeling import movinet_layers
-from official.projects.movinet.modeling import movinet_model
-from official.projects.movinet.tools import export_saved_model
 from collections import deque
-import cv2
 
 
+# Load TFLite Model
 model_id = 'a1'
-
 # Create the interpreter and signature runner
 interpreter = tf.lite.Interpreter(model_path=f'movinet_{model_id}_stream.tflite')
 runner = interpreter.get_signature_runner()
@@ -51,7 +24,7 @@ original_video_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = cap.get(cv2.CAP_PROP_FPS)
 
 # Write Video
-save = True
+save = False
 if save:
     out_vid = cv2.VideoWriter(f'output.mp4', 
                          cv2.VideoWriter_fourcc(*'mp4v'),
@@ -65,14 +38,6 @@ with tf.io.gfile.GFile("ufc101_label_map.txt") as f:
     lines = f.readlines()
     KINETICS_600_LABELS_LIST = [line.strip() for line in lines]
     KINETICS_600_LABELS = tf.constant(KINETICS_600_LABELS_LIST)
-
-def load_gif(file_path, image_size=(172, 172)):
-    """Loads a gif file into a TF tensor."""
-    with tf.io.gfile.GFile(file_path, 'rb') as f:
-        video = tf.io.decode_gif(f.read())
-    video = tf.image.resize(video, image_size)
-    video = tf.cast(video, tf.float32) / 255.
-    return video
 
 def get_top_k(probs, k=5, label_map=KINETICS_600_LABELS):
     """Outputs the top k model labels and probabilities on the given video."""
@@ -91,10 +56,11 @@ while True:
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     frames_queue.append(img_rgb)
     if len(frames_queue) == 8:
-        # img_resize = cv2.resize(img, image_size)
-        img_resize = tf.image.resize(frames_queue, image_size)
+
+        img_to_tensor = tf.convert_to_tensor(frames_queue, dtype=tf.uint8)
+        # print(type(img_to_tensor))
+        img_resize = tf.image.resize(img_to_tensor, image_size)
         img_norm = tf.cast(img_resize, tf.float32) / 255.
-            
         clips = tf.split(img_norm[tf.newaxis], img_norm.shape[0], axis=1)
 
         # To run on a video, pass in one frame at a time
@@ -116,32 +82,11 @@ while True:
     if save:
         out_vid.write(img)
 
-    # cv2.imshow('img', img)
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap.release()
 if save:
     out_vid.release()
-# cv2.destroyAllWindows()
-
-
-
-# # Insert your video clip here
-# video = load_gif('jumpingjack.gif', image_size=(172, 172))
-# clips = tf.split(video[tf.newaxis], video.shape[0], axis=1)
-
-# # To run on a video, pass in one frame at a time
-# states = init_states
-# for clip in clips:
-#     # Input shape: [1, 1, 172, 172, 3] ---> 224, 224 for a2 version
-#     outputs = runner(**states, image=clip)
-#     logits = outputs.pop('logits')[0]
-#     states = outputs
-
-# probs = tf.nn.softmax(logits)
-# top_k = get_top_k(probs)
-# print()
-# for label, prob in top_k:
-#     print(label, prob)
-
+cv2.destroyAllWindows()
